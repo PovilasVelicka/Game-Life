354cfed31adf8324ea50c32c317eaa7c53cc4047
Povilas Velicka <velicka.povilas@gmail.com>
2022-09-08 18:09:29 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,26 +1,20 @@
 const _cellSize = 10;
-const _cellsCountX = 60;
-const _cellsCountY = 60;
-let _mainGameField = [];
-let _currentGeneration;
+const _cellsCountX = 100;
+const _cellsCountY = 50;
+const _gameContextBachground = 'rgba(52, 158, 0,1)';
+const _gameContextGridColor = 'rgba(255,255,255,.5)';
+const _gameFieldCellColor = 'rgba(0,0,0)'
 
-window.onload = () => {
-    const mainField = document.getElementById('gameField')
-    mainField.setAttribute('width', `${_cellsCountX * _cellSize}px`)
-    mainField.setAttribute('height', `${_cellsCountY * _cellSize}px`)
-    mainField.style = 'border: 1px solid #000000; margin: 50px auto; display: block;'
-}
+let _mainGameField = [];
+let _mainGameContext = document.getElementById('gameField')
+let _currentGeneration;
 
 function clearGameField() {
     for (let x = 0; x < _cellsCountX; x++) {
         _mainGameField[x] = []
         for (let y = 0; y < _cellsCountY; y++) {
-            _mainGameField[x][y] = false
+            _mainGameField[x][y] = false// Math.random()<.80?false:true
         }
     }
     _currentGeneration = 0
 }
-
-
-
-
